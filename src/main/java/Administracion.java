4ab8347c0f9368b866c969ce32409d84c199237c
Mateo Garzon <jhmgarzonre@unal.edu.co>
2020-10-02 12:00:37 -0500

    Creacion del menu sin considerar el ingreso de usuario

--- src/main/java/Administracion.java
+++ src/main/java/Administracion.java
@@ -1,3 +1,5 @@
 public class Administracion {
+    public static void administrar(){
 
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,39 @@ public class Main {
     public static ArrayList<Inferencial> inferenciales = new ArrayList<>();
     public static void main(String[] args) throws IOException {
 
+        boolean condicion = true;
+        while(condicion){
+            System.out.println("1. Administración.");
+            System.out.println("2. Busqueda.");
+            System.out.println("3. Diagnóstico de inconsistencias.");
+            System.out.println("4. Guardar.");
+            System.out.println("0. Salir");
+            String opcion = input.readLine();
+            if (opcion.equals("1")){
+                Administracion.administrar();
+            }else if(opcion.equals("2")){
+
+            }else if(opcion.equals("3")){
+
+            }else if(opcion.equals("4")){
+
+            }else if(opcion.equals("0")){
+                while(true){
+                    System.out.println("Al seleccionar esta opcion se perderan los cambios si no ha guardado");
+                    System.out.println("Y. Salir del programa");
+                    System.out.println("N. Permanecer en el programa");
+                    opcion = input.readLine();
+                    if (opcion.equals("Y")){
+                        condicion = false;
+                        break;
+                    }else if (opcion.equals("N")){
+                        break;
+                    }else{
+                        System.out.println("El dato ingresado no es valido");
+                    }
+                }
+            }
+        }
     }
 
     public static void Ingresar(){
