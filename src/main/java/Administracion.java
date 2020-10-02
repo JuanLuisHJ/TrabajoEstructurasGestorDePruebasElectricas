d5407165204d39f2ca539409ac691b3334dc7113
Mateo Garzon <jhmgarzonre@unal.edu.co>
2020-10-02 14:45:15 -0500

    continuación menú administración

--- src/main/java/Administracion.java
+++ src/main/java/Administracion.java
@@ -4,8 +4,7 @@ import java.io.*;
 public class Administracion {
     public static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     public static void administrar(ArrayList<Experimento> experimentos,ArrayList<Muestra> muestras,ArrayList<Modelo> modelos,ArrayList<Informe> informes,ArrayList<Priori> distribucionesapriori,ArrayList<Posterior> distribucionposterior,ArrayList<Descriptiva> descriptivas,ArrayList<Inferencial> inferenciales) throws IOException {
-        boolean condicionA = true;
-        while (condicionA){
+        while (true){
             System.out.println("1. Experimento.");
             System.out.println("2. Muestra.");
             System.out.println("3. Modelo.");
@@ -14,10 +13,29 @@ public class Administracion {
             System.out.println("6. Distribución posterior.");
             System.out.println("7. Descriptiva.");
             System.out.println("8. Inferencial.");
-            System.out.println("0. Salir");
+            System.out.println("0. Cancelar");
             String opcionA =input.readLine();
+            if (opcionA.equals("1")){
 
+            }else if(opcionA.equals("2")){
 
+            }else if(opcionA.equals("3")){
+
+            }else if(opcionA.equals("4")){
+
+            }else if(opcionA.equals("5")){
+
+            }else if(opcionA.equals("6")){
+
+            }else if(opcionA.equals("7")){
+
+            }else if(opcionA.equals("8")){
+
+            }else if(opcionA.equals("0")){
+                break;
+            }else{
+                System.out.println("Los datos ingresados no son validos");
+            }
         }
     }
 }
