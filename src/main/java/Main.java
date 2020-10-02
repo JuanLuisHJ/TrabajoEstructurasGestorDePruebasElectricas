5d8a2e991c3c0f436b29c96914a3b8afaae80d54
Mateo Garzon <jhmgarzonre@unal.edu.co>
2020-10-02 11:28:51 -0500

    creación de listas de clases

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,15 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static BufferedReader input = new BufferedReader(new InputStreamReader(System.in)); 
+    public static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    public static ArrayList<Experimento> experimentos = new ArrayList<>();
+    public static ArrayList<Muestra> muestras = new ArrayList<>();
+    public static ArrayList<Modelo> modelos = new ArrayList<>();
+    public static ArrayList<Informe> informes = new ArrayList<>();
+    public static ArrayList<Priori> distribucionesapriori = new ArrayList<>();
+    public static ArrayList<Posterior> distribucionposterior = new ArrayList<>();
+    public static ArrayList<Descriptiva> descriptivas = new ArrayList<>();
+    public static ArrayList<Inferencial> inferenciales = new ArrayList<>();
     public static void main(String[] args) throws IOException {
 
     }
