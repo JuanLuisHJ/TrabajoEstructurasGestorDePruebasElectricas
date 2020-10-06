7b139c1216f638796935029c4451ed73506905e1
Juan Luis Henao Jiménez <jlhenoj@unal.co>
2020-10-05 19:36:29 -0500

    confirmación

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 import java.io.*;
-
-
 public class Main {
     public static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     public static ArrayList<Usuario> Usuarios = new ArrayList<>();
@@ -20,29 +17,21 @@ public class Main {
     public static ArrayList<Clase> clases = new ArrayList<>();
     public static int [] IDs = new int[8];
     public static void main(String[] args) throws IOException {
-
-
-
-
-
-
-
-
         CargarDatos();
         boolean acceso = false;
         boolean seleccionsiregistro = false;
-        while(!acceso) {
-            System.out.println("Sistema de gestión en modelado estadístico:\n");
-            System.out.println("  1. Ingresar");
-            System.out.println("  2. Registrarse");
-            System.out.println("  0. Salir");
-            String seleccion="";
-            if (!seleccionsiregistro){
+        while (!acceso) {
+            String seleccion = "";
+            if (!seleccionsiregistro) {
+                System.out.println("Sistema de gestión de pruebas eléctricas:\n");
+                System.out.println("  1. Ingresar");
+                System.out.println("  2. Registrarse");
+                System.out.println("  0. Salir");
                 seleccion = input.readLine();
             }
             if (seleccion.equals("1") || seleccionsiregistro) {
-                if (seleccionsiregistro){
-                    seleccionsiregistro=false;
+                if (seleccionsiregistro) {
+                    seleccionsiregistro = false;
                 }
                 System.out.print("Ingrese correo electrónico o documento: ");
                 String ingreso = input.readLine();
@@ -50,132 +39,124 @@ public class Main {
                 try {
                     int documento = Integer.parseInt(ingreso);
                     int index = -1;
-                    for (int indice=0;indice<Usuarios.size();indice++){
-                        if (Usuarios.get(indice).Documento==documento){
-                            encontrado = true;
-                            index=indice;
-                            break;
-                        }
-                    }
-                    if (!encontrado){
-                        System.out.println("Usuario con documento: \""+documento+"\" no econtrado\n");
-                    }
-                    else {
-                        System.out.print("Ingrese contraseña: ");
-                        String contraseña = input.readLine();
-                        if (Usuarios.get(index).Contraseña.equals(contraseña)){
-                            System.out.println("Bienvenido al sistema de gestión en modelado estadístico\n");
-                            acceso=true;
-                            seleccionsiregistro=false;
-                        }
-                        else{
-                            System.out.println("Contraseña incorrecta\n");
-                        }
-                    }
-                } catch (Exception exc){
-                    int index = -1;
-                    for (int indice=0;indice<Usuarios.size();indice++){
-                        if (Usuarios.get(indice).Correo.equals(ingreso)){
+                    for (int indice = 0; indice < Usuarios.size(); indice++) {
+                        if (Usuarios.get(indice).Documento == documento) {
                             encontrado = true;
                             index = indice;
                             break;
                         }
                     }
-                    if (!encontrado){
-                        System.out.println("Usuario con correo: \""+ingreso+"\" no econtrado\n");
+                    if (!encontrado) {
+                        System.out.println("Usuario con documento: \"" + documento + "\" no econtrado\n");
+                    } else {
+                        System.out.print("Ingrese contraseña: ");
+                        String contraseña = input.readLine();
+                        if (Usuarios.get(index).Contraseña.equals(contraseña)) {
+                            System.out.println("Bienvenido al sistema de gestión de pruebas eléctricas\n");
+                            acceso = true;
+                        } else {
+                            System.out.println("Contraseña incorrecta\n");
+                        }
                     }
-                    else {
+                } catch (Exception exc) {
+                    int index = -1;
+                    for (int indice = 0; indice < Usuarios.size(); indice++) {
+                        if (Usuarios.get(indice).Correo.equals(ingreso)) {
+                            encontrado = true;
+                            index = indice;
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("Usuario con correo: \"" + ingreso + "\" no econtrado\n");
+                    } else {
                         System.out.print("Ingrese contraseña: ");
                         String contraseña = input.readLine();
 
-                        if (Usuarios.get(index).Contraseña.equals(contraseña)){
-                            System.out.println("Bienvenido al sistema de gestión en modelado estadístico\n");
-                            acceso=true;
-                            seleccionsiregistro=false;
-                        }
-                        else{
+                        if (Usuarios.get(index).Contraseña.equals(contraseña)) {
+                            System.out.println("Bienvenido al sistema de gestión de pruebas eléctricas\n");
+                            acceso = true;
+                        } else {
                             System.out.println("Contraseña incorrecta");
                         }
                     }
                 }
-            }
-            else if (seleccion.equals("2")){
+            } else if (seleccion.equals("2")) {
                 int documento = -1;
-                boolean documentovalido=false;
-                boolean cancelarregistro=false;
+                boolean documentovalido = false;
+                boolean cancelarregistro = false;
                 boolean documentoadmitido = false;
-                while(!documentoadmitido && !cancelarregistro){
+                while (!documentoadmitido && !cancelarregistro) {
                     System.out.print("Ingresar documento: ");
                     String doc = input.readLine();
-                    try{
+                    try {
                         documento = Integer.parseInt(doc);
-                        if(documento<0){
+                        if (documento < 0) {
                             System.out.println("Ingrese un documento válido: Valor numérico entero mayor que cero\n");
-                            documentovalido=false;
+                            documentovalido = false;
+                        } else {
+                            documentovalido = true;
                         }
-                        else{
-                            documentovalido=true;
-                        }
-                    } catch (Exception exc){
+                    } catch (Exception exc) {
                         System.out.println("Ingrese un documento válido: Valor numérico entero mayor que cero\n");
                         System.out.println("¿Cancelar registro?");
                         System.out.println("Y <---> SI");
                         System.out.println("N <---> NO");
                         String cancelar = input.readLine();
-                        if (cancelar.equalsIgnoreCase("y") || cancelar.equalsIgnoreCase("si")){
-                            cancelarregistro=true;
+                        if (cancelar.equalsIgnoreCase("y") || cancelar.equalsIgnoreCase("si")) {
+                            cancelarregistro = true;
                         }
                     }
-                    if (documentovalido){
+                    if (documentovalido) {
                         int contador = 0;
-                        for(Usuario usuario : Usuarios){
-                            if(usuario.Documento==documento){
-                                System.out.println("El documento ingresado ya se encuentra regstrado con el correo: \""+usuario.Correo+"\"\n");
+                        for (Usuario usuario : Usuarios) {
+                            if (usuario.Documento == documento) {
+                                System.out.println("El documento ingresado ya se encuentra regstrado con el correo: \"" + usuario.Correo + "\"\n");
                                 System.out.println("¿Cancelar registro?");
                                 System.out.println("Y <---> SI");
                                 System.out.println("N <---> NO");
                                 String cancelar = input.readLine();
-                                if (cancelar.equalsIgnoreCase("y") || cancelar.equalsIgnoreCase("si")){
-                                    cancelarregistro=true;
+                                if (cancelar.equalsIgnoreCase("y") || cancelar.equalsIgnoreCase("si")) {
+                                    cancelarregistro = true;
                                 }
                                 break;
                             }
                             contador++;
                         }
-                        if (contador==Usuarios.size()){
-                            documentoadmitido=true;
+                        if (contador == Usuarios.size()) {
+                            documentoadmitido = true;
                         }
                     }
                 }
-                if (cancelarregistro){
+                if (cancelarregistro) {
                     continue;
                 }
                 String correo = "";
                 boolean correoexitoso = false;
                 cancelarregistro = false;
-                while(!correoexitoso && !cancelarregistro){
+                while (!correoexitoso && !cancelarregistro) {
                     System.out.print("Ingresar correo: ");
                     correo = input.readLine();
                     int contador = 0;
-                    for(Usuario usuario : Usuarios){
-                        if (usuario.Correo.equalsIgnoreCase(correo)){
-                            System.out.println("El correo ingresado ya se encuentra registrado con el documento: \""+usuario.Documento+"\"\n");
+                    for (Usuario usuario : Usuarios) {
+                        if (usuario.Correo.equalsIgnoreCase(correo)) {
+                            System.out.println("El correo ingresado ya se encuentra registrado con el documento: \"" + usuario.Documento + "\"\n");
                             System.out.println("¿Cancelar registro?");
                             System.out.println("Y <---> SI");
                             System.out.println("N <---> NO");
                             String cancelar = input.readLine();
-                            if (cancelar.equalsIgnoreCase("y") || cancelar.equalsIgnoreCase("si")){
-                                cancelarregistro=true;
+                            if (cancelar.equalsIgnoreCase("y") || cancelar.equalsIgnoreCase("si")) {
+                                cancelarregistro = true;
                             }
                             break;
                         }
                         contador++;
                     }
-                    if (contador==Usuarios.size()){
-                        correoexitoso=true;
+                    if (contador == Usuarios.size()) {
+                        correoexitoso = true;
                     }
                 }
-                if (cancelarregistro){
+                if (cancelarregistro) {
                     continue;
                 }
                 System.out.print("Ingresar nombre: ");
@@ -184,7 +165,7 @@ public class Main {
                 String apellido = input.readLine();
                 System.out.print("Ingresar contraseña: ");
                 String contraseña = input.readLine();
-                Usuario nuevousuario = new Usuario(documento,correo,nombre,apellido,contraseña);
+                Usuario nuevousuario = new Usuario(documento, correo, nombre, apellido, contraseña);
                 Usuarios.add(nuevousuario);
                 try {
                     Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -192,7 +173,7 @@ public class Main {
                     gson.toJson(Usuarios, writer);
                     System.out.println("El nuevo usuario se ha registrado satisfactoriamente\n");
                     System.out.println("Usuarios registrados: \n");
-                    for(Usuario usuario : Usuarios){
+                    for (Usuario usuario : Usuarios) {
                         System.out.println(usuario);
                     }
                     System.out.println();
@@ -201,17 +182,14 @@ public class Main {
                     ex.printStackTrace();
                 }
                 System.out.println("Por favor, ingrese sus datos para acceder al sistema\n");
-                seleccionsiregistro=true;
-            }
-            else if(seleccion.equals("0")){
+                seleccionsiregistro = true;
+            } else if (seleccion.equals("0")) {
                 break;
                 // falta relacionarlo con el menu
-            }
-            else{
+            } else {
                 System.out.println("Por favor, ingrese una opción válida: \"1\", \"2\" o \"0\"");
             }
         }
-
         /*boolean condicion = true;
         while(condicion){
             System.out.println("1. Administración.");
@@ -247,12 +225,8 @@ public class Main {
         }
     }
 
-    /*public static void Ingresar(){
-        System.out.println("Ingrese por favor su correo o contraseña");
+   */
     }
-    public static void DistribucionAPriori(){
-        
-    }*/
     public static void CargarDatos(){
         try {
             Gson gson = new Gson();
